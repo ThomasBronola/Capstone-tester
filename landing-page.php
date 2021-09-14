802d8e4aae82c709d40a572855578eb4aac89279
LaczFeli <90681326+LaczFeli@users.noreply.github.com>
2021-09-14 23:37:01 +0800

    Update landing-page.php

--- landing-page.php
+++ landing-page.php
@@ -91,33 +91,33 @@
 
                         <!-- SECOND CIRCLE -->
                         <li class="list-group-item timeline-inverted">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/2.jpg"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/pakalu.jpg"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>bronolathomas@gmail.com</h4>
                                     <h4 class="subheading">Thomas Adrian U. Bronola</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                    <p class="text-muted">Don Romantiko</p>
                                 </div>
                             </div>
                         </li>
 
                         <!-- THIRD CIRCLE -->
                         <li class="list-group-item">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/pakalu.jpg"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>lacz.feliciano@gmail.com</h4>
                                     <h4 class="subheading">Eris Jericho F. Lacsamana</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                    <p class="text-muted">Anong Progress Progress?</p>
                                 </div>
                             </div>
                         </li>
                         <li class="list-group-item timeline-inverted">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/4.jpg"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/pakalu.jpg"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>perfjohngomera@gmail.com</h4>
